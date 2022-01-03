b733ea478ccf05a1082055c8af439114e52d2db7
Thinushi <thinupavani@gmail.com>
2022-01-03 22:37:37 +0530

    app bar apps icon

--- modules/LayoutComponents/AppBar.jsx
+++ modules/LayoutComponents/AppBar.jsx
@@ -1,19 +1,48 @@
-import { Box } from '@material-ui/core';
-import { withThemeCreator } from '@material-ui/styles';
-import React from 'react'
-import styles from "../../styles/Home.module.css";
+import { AppsRounded } from "@mui/icons-material";
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import { Box, IconButton, Typography } from "@mui/material";
+import Link from "next/link";
+
+const useStyles = makeStyles({
+  appBar: {
+    height: 48,
+    backgroundColor: "#0078D7",
+    zIndex: 100,
+    justifyContent: "space-between",
+  },
+  appIconBox: {
+    width: 48,
+    height: 48,
+    display: "flex",
+  },
+  appsIcon: {
+    position: "relative",
+    color: "#fff",
+  },
+  TypoName: {
+    fontFamily: "SegoeUI-Regular-final",
+    fontSize: 16,
+    color: "#fff",
+  },
+});
 
 function AppBar() {
-    return (
-        <div className={styles.appBar}>
-            {/* logo box */}
-            <div className={styles.appIconBox}>
-                
-            </div>
-            {/* search box */}
-            {/* icons box */}
-        </div>
-    )
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.appBar}>
+      {/* logo box */}
+      <Box className={classes.appIconBox}>
+        <IconButton className={classes.appsIcon}>
+          <AppsRounded />
+        </IconButton>
+        {/* <Typography className={classes.TypoName}>Outlook</Typography> */}
+      </Box>
+      {/* search box */}
+      {/* icons box */}
+    </Box>
+  );
 }
 
-export default AppBar
+export default AppBar;
