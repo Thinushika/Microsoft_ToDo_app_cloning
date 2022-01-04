3b93c3eb5dbf8798920ce3afba6d9ef05c95f410
Thinushi <thinupavani@gmail.com>
2022-01-04 22:56:49 +0530

    update

--- modules/LayoutComponents/AppBar.jsx
+++ modules/LayoutComponents/AppBar.jsx
@@ -1,8 +1,8 @@
 import { AppsRounded } from "@mui/icons-material";
 import React from "react";
 import { makeStyles } from "@mui/styles";
-import { Box, IconButton, Typography } from "@mui/material";
-import Link from "next/link";
+import { deepOrange, deepPurple } from '@mui/material/colors';
+import { Avatar, Box, IconButton, Link } from "@mui/material";
 
 const useStyles = makeStyles({
   appBar: {
@@ -10,11 +10,19 @@ const useStyles = makeStyles({
     backgroundColor: "#0078D7",
     zIndex: 100,
     justifyContent: "space-between",
-  },
-  appIconBox: {
-    width: 48,
-    height: 48,
     display: "flex",
+    flexDirection: "row",
+  },
+  appbarLeft: {
+    width: 48,
+    display: "inline",
+  },
+  appbarRight: {
+    width: 48,
+    display: "inline",
+  },
+  appbarCenter: {
+    display: "inline",
   },
   appsIcon: {
     position: "relative",
@@ -25,6 +33,12 @@ const useStyles = makeStyles({
     fontSize: 16,
     color: "#fff",
   },
+  outlookLink: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    fontFamily: "Arial, Helvetica, sans-serif",
+  },
 });
 
 function AppBar() {
@@ -33,14 +47,32 @@ function AppBar() {
   return (
     <Box className={classes.appBar}>
       {/* logo box */}
-      <Box className={classes.appIconBox}>
+      <Box className={classes.appbarLeft}>
         <IconButton className={classes.appsIcon}>
           <AppsRounded />
         </IconButton>
-        {/* <Typography className={classes.TypoName}>Outlook</Typography> */}
       </Box>
       {/* search box */}
+      <Box className={classes.appbarCenter}>
+        <Link
+          className={classes.outlookLink}
+          component="button"
+          variant="body2"
+          onclick={() => {
+            console.log("I'm a button");
+          }}
+        >
+          Outlook
+        </Link>
+      </Box>
       {/* icons box */}
+      <Box className={classes.appbarRight}>
+      <Avatar sx={{ 
+        bgcolor: deepOrange[500], 
+        width:30, 
+        height:30,
+         }}>N</Avatar>
+      </Box>
     </Box>
   );
 }
